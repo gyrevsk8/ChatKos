f734e6561bc7cf8ab6ad8aef5750acd0b5a67280
gyrevsk8 <gyrevsk8@gmail.com>
2022-10-28 11:53:21 +0300

    Add files via upload

--- chat/src/Mss.java
+++ chat/src/Mss.java
@@ -1,4 +1,5 @@
 import java.io.PrintStream;
+import java.util.Date;
 import java.util.Scanner;
 import java.io.*;
 public class Mss {
@@ -19,13 +20,19 @@ public class Mss {
 
 
         try(FileWriter writer = new FileWriter("Log.txt", true))
-        {
+        {   Date date = new Date();
+            writer.append('\n');
             // запись всей строки
+            writer.write(date.toString());
+            writer.append('\n');
             String text = "Name:"+ Project.Chat.getName()+ " Surneme:" +Project.Chat.getSurname() + " Messge:";
             writer.write(text);
             text = messge;
             writer.write(text);
             writer.append('\n');
+
+
+
 
             writer.flush();
         }
