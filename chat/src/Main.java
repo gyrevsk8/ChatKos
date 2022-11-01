d75bacc6fb122cc8c9382ddb7b702433328be4b6
Jewsus Christ <gyrevsk8@gmail.com>
2022-11-01 09:20:01 +0300

    &

--- chat/src/Main.java
+++ chat/src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main (String[] args) {
-        Project big = new Project();
+        Project big = new Project();//cocking dick
         big.log();
         
     }
