9441d98e120e3ebe8fa2841ed1cdf95def4357ca
Jewsus Christ <gyrevsk8@gmail.com>
2022-11-01 09:26:48 +0300

    check

--- chat/src/Main.java
+++ chat/src/Main.java
@@ -1,7 +1,9 @@
 
 
 public class Main {
+
     public static void main (String[] args) {
+        System.out.println(" ????? ");
         Project big = new Project();//cocking dick
         big.log();
         
