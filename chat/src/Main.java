f027527f1a63ec79b05deed5f5c6b5029845047c
Jewsus Christ <gyrevsk8@gmail.com>
2022-11-07 16:47:19 +0300

    v0.22b

--- chat/src/Main.java
+++ chat/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main (String[] args) {
         //некст пара будет сервер 01.11
         // 12 31 323
+        //asdfghj
         Project big = new Project();
         big.log();
         //ghjk
