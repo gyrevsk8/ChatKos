c3a7d21d1583f72f78cacbe93d6840b4609f958a
Антон <antonkonakov3@gmail.com>
2022-11-07 16:45:53 +0300

    test 2

--- chat/src/Main.java
+++ chat/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main (String[] args) {
         //некст пара будет сервер 01.11
+        // 12 31 323
         Project big = new Project();
         big.log();
         //ghjk
